e7f052bae9945fb90228bfcb7786feb356697401
Vasudevan Sampath <vasudevan-sampath@hpe.com>
2020-12-01 13:21:44 +0530

    BG Image

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function listpage()
 }
 function formspage()
 {
+    document.getElementById("sand").style = "background-image: url('src/bg7.jpg');background-size : auto";
     document.getElementById("forms").style.display = "block";
     document.getElementById("gform").src = "https://docs.google.com/forms/d/e/1FAIpQLScDeJ1_zY0hjb38itMQz2x3AfxcBAz_byBy8UVkiCr9u54eIg/viewform?embedded=true";
     document.getElementById("initialpage").style.display = "none";
@@ -19,5 +20,6 @@ function gotoHomePage()
    document.getElementById("initialpage").style.display = "block";
    document.getElementById("k").style.display = "none";
    document.getElementById("variable").innerHTML = 0;
+   document.getElementById("sand").style = "background-image: url('src/bg7.jpg');background-size :100vw 100vh";
 }
 
